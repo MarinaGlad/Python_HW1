feb577610bf7c52212424ea09319e029ff555f33
MarinaGlad <112165621+MarinaGlad@users.noreply.github.com>
2022-11-22 16:36:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,37 +5,37 @@
 # - 7 -> да
 # - 1 -> нет
 
-# number_day = int(input('Введите номер дня недели: '))
-# if 0 < number_day < 6:
-#     print(f'{number_day} - это будний день')
-# elif 5 < number_day < 8:
-#     print(f'{number_day} - выходной')
-# else:
-#     print(f'{number_day} - судный день')
+number_day = int(input('Введите номер дня недели: '))
+if 0 < number_day < 6:
+     print(f'{number_day} - это будний день')
+elif 5 < number_day < 8:
+     print(f'{number_day} - выходной')
+else:
+     print(f'{number_day} - судный день')
 
 # 2. Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z
 # для всех значений предикат.
 
-# x = int(input('Введите число x: '))
-# y = int(input('Введите число y: '))
-# z = int(input('Введите число z: '))
-#
-# a = x * y * z
-# b = x + y + z
-#
-# if a > 0:
-#     a = 0
-# elif a < 1:
-#     a = 1
-# if b > 0:
-#     b = 1
-# elif b < 1:
-#     b = 1
-#
-# if a == b:
-#     print('Утверждение истинно')
-# elif a != b:
-#     print('Утверждение ложно')
+x = int(input('Введите число x: '))
+y = int(input('Введите число y: '))
+z = int(input('Введите число z: '))
+
+a = x * y * z
+b = x + y + z
+
+if a > 0:
+    a = 0
+elif a < 1:
+    a = 1
+if b > 0:
+    b = 1
+elif b < 1:
+    b = 1
+
+if a == b:
+    print('Утверждение истинно')
+elif a != b:
+    print('Утверждение ложно')
 
 
 # 3. Напишите программу, которая принимает на вход координаты точки (X и Y),
@@ -45,15 +45,15 @@
 # - x=2; y=4-> 1
 # - x=-34; y=-30 -> 3
 
-# X, Y = int(input('Введите координаты точки X: ')), int(input('Введите координаты точки Y: '))
-# if X > 0 and Y >0:
-#     print('I четверть')
-# elif X < 0 and Y > 0:
-#     print('II четверть')
-# elif X < 0 and Y < 0:
-#     print('III четверть')
-# else:
-#     print('IV четверть')
+X, Y = int(input('Введите координаты точки X: ')), int(input('Введите координаты точки Y: '))
+if X > 0 and Y >0:
+    print('I четверть')
+elif X < 0 and Y > 0:
+    print('II четверть')
+elif X < 0 and Y < 0:
+    print('III четверть')
+else:
+    print('IV четверть')
 
 
 # 4. Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных
